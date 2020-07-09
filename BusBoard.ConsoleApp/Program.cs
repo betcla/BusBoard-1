aaa46a79325fec27fbe80a948486b2349b87a32b
daniel347 <daniel.siromani@softwire.com>
2020-07-09 15:12:23 +0100

    Basic REST call

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BusBoard.ConsoleApp
 {
@@ -10,6 +13,15 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
+      ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+      var client = new RestClient("https://api.tfl.gov.uk");
+
+      var request = new RestRequest("StopPoint/Mode/bus/Disruption", DataFormat.Json);
+      request.AddParameter("app_id", "7d0152bc");
+      request.AddParameter("app_key", "9c228e7ec36f4500b944f2d79bf9d16f");
+
+      var response = client.Get(request);
     }
   }
 }
