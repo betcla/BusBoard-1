91fa88d7dc79bd8132364f455d2377a86a618615
daniel347 <daniel.siromani@softwire.com>
2020-07-13 17:46:35 +0100

    Designed BusInfo view

--- BusBoard.ConsoleApp/Properties/Program.cs
+++ BusBoard.ConsoleApp/Properties/Program.cs
@@ -17,24 +17,24 @@ namespace BusBoard.ConsoleApp
         static void Main(string[] args)
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            
+
             //var stopCode = "490008660N";
             //var postcode = "NW51TL";
             Console.WriteLine("Welcome, please enter a postcode (Hint: choose NW51TL)");
             var postcode = Console.ReadLine();
-            
+
             var tflReader = new TFLAPIReader();
             var postcodesReader = new PostcodesAPIReader();
             var tflCaller = new TFLAPICaller();
             var postcodesCaller = new PostcodesAPICaller();
-            
+
             var loc = postcodesReader.GetLocation(postcodesCaller.GetLocationFromPostcode(postcode));
 
             var stops = tflReader.GetStops(tflCaller.GetStopCodeFromLocation(loc));
             foreach (var stop in stops.Take(2))
             {
                 var response = tflCaller.GetBusesFromStopCode(stop.naptanID);
-                
+
                 stop.Arrivals = tflReader.GetArrivals(response);
                 Console.WriteLine($"Next 5 buses at stop {stop.commonName}:");
                 foreach (var bus in stop.Arrivals.Take(5))
